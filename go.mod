c80aee0035b5298f937053842fac6a898064938a
Mehdi BEN ABDALLAH <mehdi.benabdallah@powerup-technology.com>
2020-04-19 19:22:16 +0200

    [fix #13] make the cli resilient to helm charfile schema changes

--- go.mod
+++ go.mod
@@ -8,9 +8,11 @@ require (
 	github.com/cpuguy83/go-md2man v1.0.4 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/golang/protobuf v0.0.0-20171021043952-1643683e1b54 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/krishicks/yaml-patch v0.0.10
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/onsi/ginkgo v1.12.0 // indirect
+	github.com/onsi/gomega v1.9.0 // indirect
 	github.com/russross/blackfriday v0.0.0-20151117072312-300106c228d5 // indirect
 	github.com/shurcooL/sanitized_anchor_name v0.0.0-20151028001915-10ef21a441db // indirect
 	github.com/spf13/cobra v0.0.2
@@ -18,6 +20,7 @@ require (
 	github.com/stretchr/testify v1.5.1 // indirect
 	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v2 v2.2.4
 	k8s.io/apimachinery v0.0.0-20180103014849-68f9c3a1feb3 // indirect
 	k8s.io/helm v2.8.2+incompatible
 )
--- pkg/chartfile/chartfile.go
+++ pkg/chartfile/chartfile.go
@@ -1,15 +1,19 @@
 package chartfile
 
 import (
+	"fmt"
+	"io/ioutil"
 	"path/filepath"
 
+	"github.com/Masterminds/semver"
+
+	yamlpatch "github.com/krishicks/yaml-patch"
 	"k8s.io/helm/pkg/chartutil"
-	"k8s.io/helm/pkg/proto/hapi/chart"
 )
 
-// Save a chart's manifest file
-func Save(cm *chart.Metadata, chartDir string) error {
-	return chartutil.SaveChartfile(Path(chartDir), cm)
+type ChartFile struct {
+	path    string
+	version string
 }
 
 // Path of a chart's manifest file
@@ -17,7 +21,68 @@ func Path(chartDir string) string {
 	return filepath.Join(chartDir, chartutil.ChartfileName)
 }
 
-// Load a chart's manifest file
-func Load(chartDir string) (*chart.Metadata, error) {
-	return chartutil.LoadChartfile(Path(chartDir))
+// Open a chart's manifest
+func Open(chartDir string) (*ChartFile, error) {
+	path := Path(chartDir)
+	metadata, err := chartutil.LoadChartfile(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &ChartFile{
+		path:    path,
+		version: metadata.Version,
+	}, nil
+}
+
+// Version returns the chart version
+func (c *ChartFile) Version() string {
+	return c.version
+}
+
+// SetVersion patches the chartfile with the new chart version
+func (c *ChartFile) SetVersion(version string) error {
+	_, err := semver.NewVersion(version)
+	if err != nil {
+		return err
+	}
+
+	c.version = version
+
+	patch, err := replaceOperation("/version", version)
+	if err != nil {
+		return err
+	}
+	return c.apply(patch)
+}
+
+func replaceOperation(path string, value string) (yamlpatch.Patch, error) {
+	op := fmt.Sprintf(`---
+- op: replace
+  path: %s
+  value: %s
+`, path, value)
+
+	ops := []byte(op)
+
+	return yamlpatch.DecodePatch(ops)
+}
+
+func (c *ChartFile) apply(patch yamlpatch.Patch) error {
+	doc, err := ioutil.ReadFile(c.path)
+	if err != nil {
+		return err
+	}
+
+	bs, err := patch.Apply(doc)
+	if err != nil {
+		return fmt.Errorf("applying patch failed: %s", err)
+	}
+
+	if err := ioutil.WriteFile(c.path, bs, 0777); err != nil {
+		return err
+	}
+
+	return nil
 }
